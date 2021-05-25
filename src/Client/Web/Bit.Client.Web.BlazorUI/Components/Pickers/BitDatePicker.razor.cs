38fba3faa7b0230c113dcb0408a8741a9b85d88a
artav <artav.work@outlook.com>
2021-05-25 23:58:59 +0430

    Add date picker files #263

---
+++ src/Client/Web/Bit.Client.Web.BlazorUI/Components/Pickers/BitDatePicker.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Bit.Client.Web.BlazorUI
+{
+    public partial class BitDatePicker
+    {
+        [Parameter] public bool IsOpen { get; set; } = false;
+
+        protected override string RootElementClass { get; } = "bit-dtp";
+
+        protected override void RegisterComponentClasses()
+        {
+            ClassBuilder.Register(() => IsEnabled is false
+                ? $"{RootElementClass}-disabled-{VisualClassRegistrar()}" : string.Empty);
+
+            ClassBuilder.Register(() => IsOpen is false
+                ? $"{RootElementClass}-open-{VisualClassRegistrar()}" : string.Empty);
+        }
+    }
+}
